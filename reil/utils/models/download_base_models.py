c466614f9c4237bd8270fc31e695eaf94972f967
XiaofengLin7 <xfl199801@gmail.com>
2025-03-24 13:18:48 -0400

    download base model script

---
+++ reil/utils/models/download_base_models.py
@@ -0,0 +1,21 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", type=str, default="Qwen/Qwen2.5-3B-Instruct")
+    parser.add_argument("--save_dir", type=str, default="./models/rlft")
+    args = parser.parse_args()
+
+    model = AutoModelForCausalLM.from_pretrained(args.model_name, cache_dir=args.save_dir)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name, cache_dir=args.save_dir)
+
+    print(f"You saved the model {args.model_name} in {args.save_dir}")
+
+
+    
+
+
+if __name__ == "__main__":
+    main()
+
