d2ff088e2355c5fda4b24b7a341a528ac290f49d
XiaofengLin7 <xfl199801@gmail.com>
2025-04-19 16:50:11 -0400

    fix dataset issue with converting prompts from list[dict] to str

---
+++ reil/scripts/run_qwen_15_peft.sh
@@ -0,0 +1,34 @@
+# Tested with 2 & 4 GPUs
+eval "$(conda shell.bash hook)"
+conda activate reil || exit 1
+
+set -x
+
+# Shift the arguments so $@ refers to the rest
+shift 2
+N_GPUS=1
+DATA_DIR="./data/sokoban_one_horizon/sft"
+BASE_MODEL="./models/rlft/models--Qwen--Qwen2.5-1.5B/snapshots/8faed761d45a263340a0528343f099c05c9a4323"
+EXPERIMENT_NAME="sokoban-1.5b-sft-qwen-2.5-1.5b-base"
+
+torchrun --standalone --nnodes=1 --nproc_per_node=$N_GPUS \
+     -m verl.trainer.fsdp_sft_trainer \
+    data.train_files=$DATA_DIR/train.parquet \
+    data.val_files=$DATA_DIR/test.parquet \
+    data.prompt_key=prompt \
+    data.response_key=response \
+    optim.lr=1e-4 \
+    data.micro_batch_size_per_gpu=4 \
+    model.partial_pretrain=$BASE_MODEL \
+    trainer.project_name=sokoban-1.5b-sft \
+    trainer.experiment_name=$EXPERIMENT_NAME \
+    trainer.default_local_dir=checkpoints/sft/$EXPERIMENT_NAME \
+    trainer.logger=['console'] \
+    trainer.total_epochs=1 \
+    trainer.default_hdfs_dir=null $@ \
+    model.lora_rank=32\
+    model.lora_alpha=16 \
+    model.target_modules=all-linear 2>&1 | tee checkpoints/sft/$EXPERIMENT_NAME/train.log
+
+    # Or you can do this:
+    # model.target_modules=[q_proj,v_proj] \
