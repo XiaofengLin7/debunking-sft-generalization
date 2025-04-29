0907d675f1d6cbf653b24ec08a01d162fa7252a2
XiaofengLin7 <xfl199801@gmail.com>
2025-04-29 10:19:24 -0400

    add sokoban env

--- reil/evaluation/eval.py
+++ reil/evaluation/eval.py
@@ -2,6 +2,8 @@ import hydra
 from reil.trainer.llm_agent.agent_proxy import VllmWrapperWg, LLMAgentProxy
 from transformers import AutoTokenizer
 import os
+from reil.env.sokoban.env import SokobanEnvReil
+
 @hydra.main(config_path="../trainer/config", config_name="evaluation.yaml")
 def main(config):
     run_eval(config)
@@ -12,6 +14,8 @@ def run_eval(config):
     actor_wg = VllmWrapperWg(config, tokenizer)
     proxy = LLMAgentProxy(config, actor_wg, tokenizer)
 
+    env = SokobanEnvReil(dim_room=(config.env.dim_x, config.env.dim_y), num_boxes=config.env.num_boxes, max_steps=config.env.max_steps, search_depth=config.env.search_depth)
+    obs = env.reset(seed=42)
     for i in range(config.max_turn):
         print(f"Turn {i+1}")
     
