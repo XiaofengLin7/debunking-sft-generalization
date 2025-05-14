10c8bfc3fe5f755f838730c07f083e6d44b650f5
XiaofengLin7 <xfl199801@gmail.com>
2025-05-14 10:05:34 -0400

    scripts files updated

--- reil/scripts/run_qwen_15_sft.sh
+++ reil/scripts/run_qwen_15_sft.sh
@@ -6,11 +6,14 @@ set -x
 
 # Shift the arguments so $@ refers to the rest
 shift 2
-N_GPUS=4
+N_GPUS=2
 
 DATA_DIR="./data/alfworld_task_type/sft"
 BASE_MODEL="./models/rlft/models--Qwen--Qwen2.5-1.5B/snapshots/8faed761d45a263340a0528343f099c05c9a4323"
 EXPERIMENT_NAME="alfworld-1.5b-pick_n_place-sft-qwen-2.5-base-full-sft"
+
+export VLLM_WORKER_MULTIPROC_METHOD="spawn"
+export ALFWORLD_DATA="/projectnb/replearn/xfl/Retriever/src/envs/alf_world/data_storage"
 
 torchrun --standalone --nnodes=1 --nproc_per_node=$N_GPUS \
      -m reil.trainer.fsdp_sft_trainer \
@@ -20,18 +23,20 @@ torchrun --standalone --nnodes=1 --nproc_per_node=$N_GPUS \
     data.response_key=response \
     data.max_length=2048 \
     data.train_batch_size=256 \
+    data.max_response_length=200 \
     optim.lr=1e-4 \
-    data.micro_batch_size_per_gpu=16 \
+    data.micro_batch_size_per_gpu=8 \
     model.partial_pretrain=$BASE_MODEL \
-    model.fsdp_config.cpu_offload=True \
-    ulysses_sequence_parallel_size=2 \
+    model.fsdp_config.cpu_offload=False \
+    ulysses_sequence_parallel_size=1 \
     use_remove_padding=True \
     model.enable_gradient_checkpointing=True \
+    trainer.policy_eval=True \
     trainer.project_name=REIL-sft \
     trainer.experiment_name=$EXPERIMENT_NAME \
     trainer.default_local_dir=checkpoints/ds543/sft/$EXPERIMENT_NAME \
-    trainer.logger="['console', 'wandb']" \
-    trainer.total_epochs=30 \
+    trainer.logger="['console']" \
+    trainer.total_epochs=2 \
     trainer.default_hdfs_dir=null $@ | tee checkpoints/ds543/sft/${EXPERIMENT_NAME}_train.log
 
     # Or you can do this:
