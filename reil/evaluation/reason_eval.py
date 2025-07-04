fe54d1fcca9d0466acdd11b4c297dcfa9b633e83
XiaofengLin7 <xfl199801@gmail.com>
2025-07-04 17:53:58 -0400

    multiple generations with one prompt, generations written into a jsonl file

---
+++ reil/evaluation/reason_eval.py
@@ -0,0 +1,106 @@
+import argparse
+from vllm import LLM, SamplingParams
+import os
+from datasets import load_dataset
+from tqdm import tqdm
+import json
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, required=True)
+    parser.add_argument("--output_dir", type=str, required=True)
+    parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--datasets", type=str, required=True)
+    parser.add_argument("--num_generation", type=int, default=10)
+    # parser.add_argument("--verifier_path", type=str, required=True)
+    parser.add_argument("--temperature", type=float, default=1.0)
+    parser.add_argument("--top_p", type=float, default=1.0)
+    parser.add_argument("--top_k", type=int, default=-1)
+    parser.add_argument("--max_tokens", type=int, default=1024)
+    parser.add_argument("--num_gpus", type=int, default=1)
+    parser.add_argument("--split", type=str, default="train")
+    args = parser.parse_args()
+
+    print(args)
+    if not os.path.exists(args.model_path):
+        print(f"Model {args.model_path} not found. Skip.")
+        return
+
+    # Load the model and tokenizer
+    print(f"Loading model {args.model_path}")
+    llm = LLM(args.model_path, tensor_parallel_size=args.num_gpus, dtype="bfloat16", gpu_memory_utilization=0.6, trust_remote_code=True)
+    sampling_params = SamplingParams(
+        n=args.num_generation, 
+        temperature=args.temperature, 
+        top_p=args.top_p, 
+        top_k=args.top_k,
+        max_tokens=args.max_tokens,
+    )
+
+    datasets = args.datasets.split(",")
+    for dataset_name in datasets:
+        dataset = load_dataset(dataset_name, split=args.split)
+        if "sokoban" in dataset_name.lower():
+            answer_key = "reward_model"
+            prompt_key = "prompt"
+        else:
+            answer_key = "answer"
+            prompt_key = "prompt"
+
+        output_file = dataset_name.split("/")[-1] + '-' + args.split + '-temp_' + str(args.temperature) + "-top_p_" + str(args.top_p) + "-top_k_" + str(args.top_k) + '.jsonl'
+        output_dir = args.output_dir
+        local_rank = int(os.getenv("LOCAL_RANK", "0"))
+        if local_rank == 0 and os.path.exists(os.path.join(output_dir, output_file)):
+            raise FileExistsError(f"Output file {output_file} already exists.")
+        # Create a JSONL file to store the output
+        with open(os.path.join(output_dir, output_file), 'w') as f:
+            for i in tqdm(range(0, len(dataset), args.batch_size)):
+                batch = dataset[i:i + args.batch_size]
+                print(batch[prompt_key][0][0])
+                inputs = [batch[prompt_key][j][0]["content"] for j in range(len(batch[prompt_key]))]
+                answers = batch[answer_key]
+
+                # Generate the answer
+                outputs = llm.generate(inputs, sampling_params=sampling_params, use_tqdm=True)
+                results = [[_.outputs[l].text for l in range(len(_.outputs))] for _ in outputs]
+                assert len(results[0]) == args.num_generation, f"Number of generations is not equal to {args.num_generation}, got {len(results[0])}"
+
+                # Prepare all outputs for batch tokenization
+                flat_outputs = []
+                output_mapping = []  # To map back to original indices
+                
+                for j in range(len(results)):
+                    for k in range(args.num_generation):
+                        flat_outputs.append(results[j][k])
+                        output_mapping.append((j, k))
+
+                # Process the results
+                output_idx = 0
+                for j, (inp, q, a, r) in enumerate(zip(inputs, batch[prompt_key], answers, results)):
+                    for k in range(args.num_generation):
+                        qa_pair = {
+                            "prompt": inp,
+                            "vanilla_response": r[k],
+                            "question": q,
+                            "answer": a,
+                            "question_id": i + j,
+                            "generation_id": k,
+                        }
+                        qa_pair["response"] = r[k]
+                        output_idx += 1
+                        # if "math" in dataset_name.lower():
+                        #     gold_answer = extract_answer_math(a)
+                        #     pred_answer = extract_answer_math(qa_pair["response"])
+                        # elif "amc23" in dataset_name.lower() or "aime" in dataset_name.lower():
+                        #     gold_answer = a
+                        #     pred_answer = extract_answer_math(qa_pair["response"])
+                        # # qa_pair["label"] = pred_answer == gold_answer
+                        # qa_pair["label"] = math_equal(pred_answer, gold_answer, timeout=True)
+                        # qa_pair["gold_answer"] = gold_answer
+                        # qa_pair["pred_answer"] = pred_answer
+                        f.write(json.dumps(qa_pair) + '\n')
+                f.flush()        
+if __name__ == "__main__":
+    main()
---
+++ reil/scripts/reason_eval.sh
@@ -0,0 +1,18 @@
+BASE_MODEL="./models/rlft/models--Qwen--Qwen2.5-1.5B/snapshots/8faed761d45a263340a0528343f099c05c9a4323"
+DATASETS="Xiaofeng77/small_sokoban"
+OUTPUT_DIR="./results"
+NUM_GENERATION=10
+N_GPUS=1
+BATCH_SIZE=256
+
+python -m reil.evaluation.reason_eval \
+    --model_path $BASE_MODEL \
+    --datasets $DATASETS \
+    --output_dir $OUTPUT_DIR \
+    --num_generation $NUM_GENERATION \
+    --num_gpus $N_GPUS \
+    --batch_size $BATCH_SIZE \
+    --temperature 1.0 \
+    --top_p 1.0 \
+    --top_k -1 \
+    --split "train"
